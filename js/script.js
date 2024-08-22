bc056455e7205dd0a5c056b873b7930b27fa9693
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-08-21 20:56:19 -0600

    c

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
 
+
+
     $(document).on("keypress", ".lineEnd", function(e){
         var key = (e.keyCode || e.which);
         if(key == 13 || key == 3) {
@@ -20,7 +22,25 @@ $(document).ready(function(){
                     <input class="lineEnd" type="text" autofocus style="border: none; background-color: #232323; width: 75%;" /></div>
                         `).insertAfter($(".cursor").last());
 
+                    var element = document.getElementById("terminal");
+                    element.scrollTop = element.scrollHeight;
+                    $("input").select().focus().blur(function() {
+                        setTimeout(function() { $("input").focus(); }, 0);
+                    });
+                }
 
+                else{
+                    $(`
+                        <div class="cursor"><span style="color: chartreuse;padding-right: .0%;">TuxTerminal@KarsonHodge:~$</span>
+                    <input class="lineEnd" type="text" autofocus style="border: none; background-color: #232323; width: 75%;" /></div>
+                        `).insertAfter($(".cursor").last());
+
+                    var element = document.getElementById("terminal");
+                    element.scrollTop = element.scrollHeight;
+
+                    $("input").select().focus().blur(function() {
+                        setTimeout(function() { $("input").focus(); }, 0);
+                    });
                 }
 
 
