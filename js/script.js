0af0b3343b3dc356de723bffc1de988bcc9bed0e
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-08-21 20:44:03 -0600

    c

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
 
 
+    $(document).on("keypress", ".lineEnd", function(e){
+        var key = (e.keyCode || e.which);
+        if(key == 13 || key == 3) {
+            $(this).each(function() {
+                $("<span />", { text: this.value, "class":"view" }).insertAfter(this);
+                $(this).hide();
+                if(this.value === "exit"){
+                    console.log("exit")
+                }
+                else if(this.value === "help"){
+
+                    $(`<div>
+                        type 'resume' for a text based resume to be shown
+                         
+                        </div>
+                        <div class="cursor"><span style="color: chartreuse;padding-right: .5%;">TuxTerminal@KarsonHodge:~$</span>
+                    <input class="lineEnd" type="text" autofocus style="border: none; background-color: #232323; width: 75%;" /></div>
+                        `).insertAfter($(".cursor")).last();
+
+
+                }
+
+
+
+
+
+            });
+        }
+    })
 
 
     const app = Vue.createApp({
@@ -12,13 +41,16 @@ $(document).ready(function(){
         methods: {
             writeText() {
                 this.text = 'Hello World!'
-            }
+            },
+
         }
     })
     app.mount('#app')
 
 
 
-
+    $("input").select().focus().blur(function() {
+        setTimeout(function() { $("input").focus(); }, 0);
+    });
 
 })
